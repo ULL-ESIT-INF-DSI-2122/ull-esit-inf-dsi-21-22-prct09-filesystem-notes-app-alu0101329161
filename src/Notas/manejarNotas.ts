31b81b17259df48a2868d4e46c5ac179db13dbb6
Alu0101329161 <alu0101329161@ull.edu.es>
2022-04-22 19:06:58 +0000

    EAñadiendo espaciado y formateo

--- src/Notas/manejarNotas.ts
+++ src/Notas/manejarNotas.ts
@@ -7,17 +7,24 @@ export class ManejarNotas {
   añadirNota(usuario: string, titulo: string, cuerpo: string, color: typeColor) {
     if (!fs.existsSync(`BaseDatosNotas/${usuario}`)) {
       console.log(`${usuario} directory will be created`);
+
       fs.mkdirSync(`BaseDatosNotas/${usuario}`, {
+
         recursive: true,
+
       });
     }
     const nota = new Nota(titulo, cuerpo, color);
+
     if (!fs.existsSync(`BaseDatosNotas/${usuario}/${titulo}.json`)) {
       fs.writeFileSync(`BaseDatosNotas/${usuario}/${titulo}.json`, new PrintNota(nota).print());
+
       console.log(chalk.green("Nueva nota añadida correctamente!"));
+
       return true;
     } else {
       console.log(chalk.red("Ya existe una nota con ese título"));
+
       return false;
     }
   }
@@ -25,10 +32,12 @@ export class ManejarNotas {
   eliminarNota(usuario: string, titulo: string) {
     if (fs.existsSync(`BaseDatosNotas/${usuario}/${titulo}.json`)) {
       fs.rmSync(`BaseDatosNotas/${usuario}/${titulo}.json`);
+
       console.log(chalk.green("Nota eliminada correctamente!"));
       return true;
     } else {
       console.log(chalk.red("Nota no encontrada pruebe otra"));
+
       return false;
     }
   }
@@ -37,10 +46,12 @@ export class ManejarNotas {
     if (fs.existsSync(`BaseDatosNotas/${usuario}/${titulo}.json`)) {
       const nota = new Nota(titulo, cuerpo, color);
       fs.writeFileSync(`BaseDatosNotas/${usuario}/${titulo}.json`, new PrintNota(nota).print());
+
       console.log(chalk.green("Nota modificada correctamente!"));
       return true;
     } else {
       console.log(chalk.red("Nota no encontrada pruebe otra"));
+
       return false;
     }
   }
@@ -52,11 +63,13 @@ export class ManejarNotas {
         const vaciarContenido = fs.readFileSync(`BaseDatosNotas/${usuario}/${notas}`);
         const stringNota = JSON.parse(vaciarContenido.toString());
         const nota = new Nota(stringNota.titulo, stringNota.cuerpo, stringNota.color);
+
         console.log(chalk.keyword(nota.getColor())(nota.getTitulo()));
       });
       return true;
     } else {
       console.log(chalk.red("No se ha encontrado al usuario en la base de Datos"));
+
       return false;
     }
   }
@@ -66,12 +79,17 @@ export class ManejarNotas {
       const vaciarContenido = fs.readFileSync(`BaseDatosNotas/${usuario}/${titulo}.json`);
       const stringNota = JSON.parse(vaciarContenido.toString());
       const nota = new Nota(stringNota.titulo, stringNota.cuerpo, stringNota.color);
+
       console.log(chalk.keyword(nota.getColor())(nota.getTitulo()));
       console.log(chalk.keyword(nota.getColor())(nota.getCuerpo()));
+
       return true;
     } else {
       console.log(chalk.red("Nota no encontrada, pruebe con otro titulo"));
+
       return false;
     }
   }
 }
+
+
